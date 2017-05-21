6a214888fa46923b8cd9e8a62bb75f224da94f51
rps <rps@jupiter.lan>
2017-05-21 17:00:11 -0500

    Add base16 color scheme, change font.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+char font[] = "Inconsolata:pixelsize=12:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
@@ -85,24 +85,24 @@ static unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#202020",
+	"#ac4142",
+	"#90a959",
+	"#f4bf75",
+	"#6a9fb5",
+	"#aa759f",
+	"#75b5aa",
+	"#e0e0e0",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#202020",
+	"#ac4142",
+	"#90a959",
+	"#f4bf75",
+	"#6a9fb5",
+	"#aa759f",
+	"#75b5aa",
+	"#e0e0e0",
 
 	[255] = 0,
 
@@ -118,7 +118,7 @@ const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
+unsigned int defaultcs = 7;
 unsigned int defaultrcs = 257;
 
 /*
