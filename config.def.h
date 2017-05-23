85874ec019e76e5c1fa8a3c22f476e50a0c0b5fa
robertseaton <robbpseaton@gmail.com>
2017-05-22 19:59:23 -0500

    Change default font to Tamsyn.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Inconsolata:pixelsize=12:antialias=true:autohint=true";
+char font[] = "Inconsolata:size=11:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
